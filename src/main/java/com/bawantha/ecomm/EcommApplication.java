3fe3c9d77f5f2523e08ec3a02a7f25ad786d2728
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-12-24 18:25:47 +0530

    project name changed

--- src/main/java/com/bawantha/ecomm/EcommApplication.java
+++ src/main/java/com/bawantha/ecomm/EcommApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.bawantha.ecomm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class EcommApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EcommApplication.class, args);
 	}
 
 }
--- src/test/java/com/bawantha/ecomm/EcommApplicationTests.java
+++ src/test/java/com/bawantha/ecomm/EcommApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.bawantha.ecomm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class EcommApplicationTests {
 
 	@Test
 	void contextLoads() {
