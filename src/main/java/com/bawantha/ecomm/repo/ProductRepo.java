d2c3f9da39fd417c8482bc367f131e0739293e48
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-12-25 16:04:24 +0530

    restart

--- src/main/java/com/bawantha/ecomm/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bawantha.ecomm.repo;
-
-import com.bawantha.ecomm.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-}
