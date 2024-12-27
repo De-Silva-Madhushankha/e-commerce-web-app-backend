5e58a25c6c572d6298cdcffa2cd14c35e9254e2a
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-12-27 18:41:43 +0530

    imperative way

--- src/main/java/com/bawantha/ecom/config/SecurityConfig.java
+++ src/main/java/com/bawantha/ecom/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,11 +19,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
         try {
             // Lambda can be replaced with method reference - AbstractHttpConfigurer::disable
-            http.csrf(customizer -> customizer.disable()); //cross site request forge
-            http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-            http.formLogin(Customizer.withDefaults()); //Only works for browser forms
-            http.httpBasic(Customizer.withDefaults()); //This for api calls (REST) , Postman
-            http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //For every req session changes because it accesses new resource and won't work with form login as expected in browser
+//            http.csrf(customizer -> customizer.disable()); //cross site request forge
+//            http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//            http.formLogin(Customizer.withDefaults()); //Only works for browser forms
+//            http.httpBasic(Customizer.withDefaults()); //This for api calls (REST) , Postman
+//            http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //For every req session changes because it accesses new resource and won't work with form login as expected in browser
+//
+
+            //BTS of lambda func
+//            Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//                @Override
+//                public void customize(CsrfConfigurer<HttpSecurity> customizer) {
+//
+//                    customizer.disable();
+//                }
+//            };
+//
+//            http.csrf(custCsrf);
 
 
 
